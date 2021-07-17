9f2b90964b9d63556bfe64ae0f47ca8bbde9e96e
anandprakashdubey <dby.anandprakash@gmail.com>
2021-07-17 17:56:45 +0100

    Add project files.

---
+++ PluralsightMovieStore.Client/Pages/Index.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace PluralsightMovieStore.Client.Pages
+{
+    public class IndexModel : PageModel
+    {
+
+        private readonly HttpClient _httpClient;
+        private readonly ILogger<IndexModel> _logger;
+        public string[] Names { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger,
+            IHttpClientFactory httpClientFactory)
+        {
+            _logger = logger;
+            _httpClient = httpClientFactory.CreateClient("PricingApiClient");
+        }       
+     
+
+        public async Task OnGet()
+        {
+            var response = await _httpClient.GetAsync("/api/names");
+            var content = await response.Content.ReadAsStringAsync();
+            Names = JsonSerializer.Deserialize<string[]>(content);
+        }                 
+    }
+}
---
+++ PluralsightPricing.API/Controllers/NamesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace PluralsightPricing.API.Controllers
+{
+   
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NamesController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new[] { Environment.MachineName };
+        }
+    }
+}
